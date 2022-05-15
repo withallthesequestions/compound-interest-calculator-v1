8bd764e713e65850cb7d2aecb57f857a44adb20c
withallthesequestions <uytpic434516@gmail.com>
2022-05-14 23:15:03 -0400

    a

--- index.js
+++ index.js
@@ -16,12 +16,24 @@ let interestInput = document.getElementById("interest");
 console.log(interestInput);
 console.log(interestInput.value);
 
-function interestCalculator(principal, interest) {
-  let result = principal * interest + Number(principal);
-  return result;
+let timeInput = document.getElementById("time");
+
+console.log(timeInput);
+console.log(timeInput.value);
+
+function interestCalculator(principal, interest, time) {
+  console.log(principal, interest, time);
+  // let simpleResult = principal * interest + Number(principal);
+  let compInt = principal * Math.pow(1 + Number(interest), time);
+  //  console.log(compInt);
+  return compInt;
 }
 
 submit.addEventListener("click", function (event) {
-  amount = interestCalculator(principalInput.value, interestInput.value);
+  amount = interestCalculator(
+    principalInput.value,
+    interestInput.value,
+    timeInput.value
+  );
   console.log(amount);
 });
